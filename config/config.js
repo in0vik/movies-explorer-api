ada5877ab41c48dbfcdac3415abbfefa052c34f6
Ilya Novik <ilya.novikk@gmail.com>
2023-02-13 21:02:58 +0800

    feat: fix env import

--- config/config.js
+++ config/config.js
@@ -3,8 +3,6 @@ const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 const { NODE_ENV = 'development' } = process.env;
 const { JWT_SECRET = 'secret' } = process.env;
 
-// JWT_SECRET = NODE_ENV === 'production' ? JWT_SECRET : 'secret'
-
 module.exports = {
   PORT,
   DB_ADDRESS,
